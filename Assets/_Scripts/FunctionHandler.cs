54b7a8d7f2345d3f457622a55609bf5e624248a7
SolidHinken <solidhinken@gmail.com>
2020-02-12 14:55:42 +0300

    12.02.20
    New ads update preparation

--- Assets/_Scripts/FunctionHandler.cs
+++ Assets/_Scripts/FunctionHandler.cs
@@ -5,6 +5,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+//using UnityEngine.Advertisments;
 
 public class FunctionHandler : Singleton<FunctionHandler>
 {
